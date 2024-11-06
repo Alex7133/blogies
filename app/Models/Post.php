92ee179c426ee1da75112d61a377026feb8956ba
AlejandroCS713 <alejandro2003carbonell@gmail.com>
2024-11-06 16:24:48 +0100

    Ejercicio 6 Completo

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,7 +10,7 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'body' , 'published_at'];
+    protected $fillable = ['title', 'body' , 'published_at','user_id'];
 
     public function user(): BelongsTo
     {
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,5 +17,12 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory(5)->create();
+        $users = User::all();
+
+        $users->each(function ($user) {
+            $user->posts()->saveMany(
+                Post::factory(10)->make()
+            );
+        });
     }
 }
